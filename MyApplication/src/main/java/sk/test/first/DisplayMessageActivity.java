b44f4527e5d0eee0aa535e0a9842b07d5b02517c
sujankh <kcsujan@gmail.com>
2014-03-08 01:07:04 +0545

    display activity displays the text passed in textbox

--- MyApplication/src/main/java/sk/test/first/DisplayMessageActivity.java
+++ MyApplication/src/main/java/sk/test/first/DisplayMessageActivity.java
@@ -1,16 +1,27 @@
 package sk.test.first;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView;
+        textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        setContentView(textView);
     }
 
 
